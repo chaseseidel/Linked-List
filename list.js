716d29a24e400effece22465f2f2c0d59b6cfce1
chaseseidel <chaseseidel16@gmail.com>
2023-07-28 12:40:45 -0700

    Create list class and create TODO functions

---
+++ list.js
@@ -0,0 +1,45 @@
+import Node from "./node";
+
+export default class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    //TODO
+  }
+
+  prepend(value) {
+    //TODO
+  }
+
+  at(index) {
+    //TODO
+  }
+
+  pop() {
+    //TODO
+  }
+
+  contains(value) {
+    //TODO
+  }
+
+  find(value) {
+    //TODO
+  }
+
+  toString() {
+    //TODO
+  }
+
+  insertAt(value, index) {
+    //TODO
+  }
+
+  removeAt(index) {
+    //TODO
+  }
+}
